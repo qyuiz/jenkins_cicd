6ba6e7576a65a8f113a966fa1ab5cf7f7228e123
qyuiz <maibatikun929@gmail.com>
2023-11-27 23:45:33 +0900

    reponame, url getfunc add

--- src/build_xml_perser.py
+++ src/build_xml_perser.py
@@ -37,6 +37,24 @@ class BuildXmlPerser:
         # revisionから先頭7文字を抜き出す
         return self.get_gerrit_trigger_parameter("GERRIT_PATCHSET_REVISION")[:7]
     
+    def get_gerrit_trigger_repo_name(self):
+        """
+        リポジトリ名称を返します
+        """
+        return self.get_gerrit_trigger_parameter("GERRIT_PROJECT")
+    
+    def get_gerrit_trigger_branch_name(self):
+        """
+        ブランチ名称を返します
+        """
+        return self.get_gerrit_trigger_parameter("GERRIT_BRANCH")
+    
+    def get_gerrit_trigger_review_url(self):
+        """
+        レビューURLを返します
+        """
+        return self.get_gerrit_trigger_parameter("GERRIT_CHANGE_URL")
+    
     def get_gerrit_trigger_parameter(self, parameter_name):
         """
         指定されたパラメータ名の値を返します
@@ -55,4 +73,5 @@ xml_file_path = "sample_data/build.xml"
 build_xml = BuildXmlPerser(xml_path=xml_file_path)
 print("comment:", build_xml.get_gerrit_trigger_review_comment())
 print("patchSetID:", build_xml.get_gerrit_trigger_patchset_id())
-print(build_xml.get_gerrit_trigger_parameter("GERRIT_PATCHSET_REVISION"))
+print("repoName:", build_xml.get_gerrit_trigger_repo_name())
+print("reviewURL:", build_xml.get_gerrit_trigger_review_url())
